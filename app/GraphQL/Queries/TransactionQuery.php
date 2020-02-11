f4ac8a3ae01ab309cb642d355bc8ef635602fef0
Saturngod <saturngod@gmail.com>
2020-02-11 15:55:21 +0630

    support transaction search and date range in graphql

--- app/GraphQL/Queries/TransactionQuery.php
+++ app/GraphQL/Queries/TransactionQuery.php
@@ -10,14 +10,27 @@ class TransactionQuery
     public function showAll($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) {
         $first = $args["first"];
         $page = $args["page"];
-        $transactions = Transaction::orderBy("date","DESC")->paginate($first,['*'],'page',$page);
-        return [
-            "data" => $transactions,
-            "page" => [
-                "currentPage" => $page,
-                "lastPage" => $transactions->lastPage()
-            ]
-        ];
+        $transactions = Transaction::orderBy("date","DESC");
+
+        $dateRange = "";
+        if(array_key_exists("dateRange",$args)) {
+            $dateRange = $args["dateRange"];
+        }
+
+        return $this->showTransactionPageData($transactions,$dateRange,$first,$page);
+    }
+
+    public function searchTransaction($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $query= $args["q"];
+        $dateRange = "";
+        $first = $args["first"];
+        $page = $args["page"];
+        if(array_key_exists("dateRange",$args)) {
+            $dateRange = $args["dateRange"];
+        }
+        $transactions = Transaction::where("name","like","%{$query}%")->orderBy("date","DESC");
+        return $this->showTransactionPageData($transactions,$dateRange,$first,$page);
     }
     public function categoryData($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
@@ -25,7 +38,41 @@ class TransactionQuery
         $catID = $args["categoryID"];
         $first = $args["first"];
         $page = $args["page"];
-        $transactions = Transaction::where("categoryID",$catID)->orderBy("date","DESC")->paginate($first,['*'],'page',$page);
+
+        $dateRange = "";
+        if(array_key_exists("dateRange",$args)) {
+            $dateRange = $args["dateRange"];
+        }
+        
+        
+        
+        $transactions = Transaction::where("categoryID",$catID)->orderBy("date","DESC");
+        return $this->showTransactionPageData($transactions,$dateRange,$first,$page);
+    }
+
+    private function fromToTransaction($dateRange,$transactions) {
+        $from = "";
+        $to = "";
+
+        if($dateRange != "") {
+            $fromTo = explode(":",$dateRange);
+            
+            if(count($fromTo) != 2) {
+                abort(400);
+            }
+            $from = $fromTo[0];
+            $to = $fromTo[1];
+        }
+
+        if($from != "" && $to != "") {
+            $transactions = $transactions->where("date",">=",$from)->where("date","<=",$to);
+        }
+        return $transactions;
+    }
+
+    private function showTransactionPageData($transactions,$dateRange,$first,$page) {
+        $transactions = $this->fromToTransaction($dateRange,$transactions);
+        $transactions = $transactions->paginate($first,['*'],'page',$page);
         return [
             "data" => $transactions,
             "page" => [
